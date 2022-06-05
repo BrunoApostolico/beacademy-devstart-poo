a3ad95bcba1c97b8ef9b0c4d03b59b153c6bd9f1
Bruno Apostolico <bapostolico@gmail.com>
2022-06-04 23:45:16 -0300

    Herança encadeada

--- modulo02/aula04/index.php
+++ modulo02/aula04/index.php
@@ -3,15 +3,17 @@
 include "Usuario.php";
 include 'Cliente.php';
 include 'Gestor.php';
+include 'GestorGeral.php';
 
 $c1 = new Cliente('bapostolico@gmail.com','123456');
 $c1->setNome('Bruno');
 $c1->setDataCadastro('01/01/2020');
 
-
 $g1 = new Gestor('priscilipe@gmail.com','12345','5000');
 $g1->setNome('Priscila');
 
+$gg1 = new GestorGeral('bapostolico@gmail.com','123','9000');
 
 var_dump($c1);
 var_dump($g1);
+var_dump($gg1);
