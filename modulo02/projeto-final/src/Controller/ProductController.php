39e7256e415e7990b2c04f6105211596415913c2
Bruno Apostolico <bapostolico@gmail.com>
2022-06-06 23:21:56 -0300

    Product

--- modulo02/projeto-final/src/Controller/ProductController.php
+++ modulo02/projeto-final/src/Controller/ProductController.php
@@ -5,8 +5,18 @@ declare(strict_types=1);
 namespace App\Controller;
 
 class ProductController{
-    public function indexAction(): void
+    public function listAction(): void
     {
-        echo 'Olá Mundo! Estou dentro de uma action, dentro do ProductController';
+        include dirname(__DIR__).'/View/product/list.php';
+    }
+
+    public function addAction():void
+    {
+        include dirname(__DIR__).'/View/product/add.php';
+    }
+
+    public function editAction():void
+    {
+        include dirname(__DIR__).'/View/product/edit.php';
     }
 }
